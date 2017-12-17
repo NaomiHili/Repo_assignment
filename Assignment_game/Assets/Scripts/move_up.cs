8373fe933eb25562ed2818031f0ece67b26700c7
NaomiHili <33513127+NaomiHili@users.noreply.github.com>
2017-12-17 17:33:04 +0100

    final_entery
    - finished level 3 made moving objects
    - fixed some code
    -created 2 new scripts so that the objects can move

---
+++ Assignment_game/Assets/Scripts/move_up.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_up : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 2), transform.position.z);
+
+    }
+}
